49d4249339824551279d8ed4eb1ff96e6bb9b096
To-Rex <torex.amaki@gmail.com>
2026-04-14 19:18:24 +0500

    cors problem

--- main.py
+++ main.py
@@ -32,11 +32,13 @@ ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://torexstore-production.up.railway.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+    #allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
